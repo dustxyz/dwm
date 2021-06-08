835b56e7dab861b51f3536dd240fa4602217b719
dustxyz <notreallydust@protonmail.com>
2021-06-08 09:26:09 -0400

    config.h: New colorscheme
    Signed-off-by: dustxyz <notreallydust@protonmail.com>

--- config.h
+++ config.h
@@ -16,8 +16,8 @@ static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static char selbordercolor[]        = "#282828";
+static char selbgcolor[]            = "#79740e";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
